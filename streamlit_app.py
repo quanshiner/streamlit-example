a19e19dace123629052e0d87da46303490cc8cb4
quanshiner <131643370+quanshiner@users.noreply.github.com>
2023-06-05 14:24:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,43 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import openai
 
-"""
-# Welcome to Streamlit!
+# 设置OpenAI API凭证
+openai.api_key = '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'
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# 定义应用程序逻辑
+def main():
+    # 设置页面标题
+    st.title('AI牧民的ChatGPT')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # 显示文本输入框和提示信息
+    input_text = st.text_area('我是你的Al助手，请在这里提问')
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # 处理按钮点击事件
+    if st.button("发送"):
+        # 创建OpenAI API调用
+        with st.spinner("正在生成回答..."):
+            # 调用OpenAI API进行聊天
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=input_text,
+                max_tokens=500,
+                temperature=0.0,
+                n=1,
+                stop=None,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+            )
 
-    points_per_turn = total_points / num_turns
+        # 从API的响应中提取模型生成的回答
+        if response.choices:
+            answer = response.choices[0].text.strip()
+            st.write("Al助手回答：")
+            st.write(answer)
+        else:
+            st.write("未能获取到回答，请稍后再试。")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# 运行应用程序
+if __name__ == '__main__':
+    main()
